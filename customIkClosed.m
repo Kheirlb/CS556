d55a8a2baccdd17eb7114b786a51555af2298ffd
kheirlb <kheirlbparks@gmail.com>
2019-11-03 21:58:44 -0800

    both thetas created

---
+++ customIkClosed.m
@@ -0,0 +1,20 @@
+function [thetaVec] = customIkClosed(x,y,l1,l2)
+t2rad1 = atan2(sqrt(1-((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2))),((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2)));
+t2rad2 = atan2(-sqrt(1-((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2))),((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2)));
+
+t2deg1 = rad2deg(t2rad1);
+t2deg2 = rad2deg(t2rad2);
+
+k1 = l1 + l2*cos(t2rad1);
+k2 = l2*sin(t2rad1);
+t1rad1 = atan2(y,x) - atan2(k2,k1);
+t1deg1 = rad2deg(t1rad1);
+
+k1 = l1 + l2*cos(t2rad2);
+k2 = l2*sin(t2rad2);
+t1rad2 = atan2(y,x) - atan2(k2,k1);
+t1deg2 = rad2deg(t1rad2);
+
+thetaVec = [t1deg1,t2deg1,t1deg2,t2deg2];
+end
+
--- testingScriptV2.m
+++ testingScriptV2.m
@@ -1,45 +1,15 @@
 clear; clc; close all;
 
-%% Creating inverse kinematics robot
-r = 100; %cm r = length 1
-s = r;     % s = length 2
-syms theta1 theta2 rob x y
-% theta, d, a, alpha
-% link angle, link offset, link length, link twist
-DH = [theta1 0 r 0; theta2 0 s 0]
-rob = SerialLink(DH)
-robfk = rob.fkine([theta1 theta2])
-xEq = robfk.t(1,1);
-yEq = robfk.t(2,1);
-preach = [160.0,9.0]
-%robik = rob.ikcon(robfk, preach)
-
-%% inverse 
+%% Initial values
 l1 = 100; %cm
 l2 = l1;
-x = -160;
-y = 9;
-t2rad1 = atan2(sqrt(1-((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2))),((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2)));
-t2rad2 = atan2(-sqrt(1-((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2))),((x^2 + y^2 - l1^2 - l2^2)/(2*l1*l2)));
+theta1low = 0;
+theta1high = 170;
+theta2low = -90;
+theta2high = 90;
 
-t2deg1 = rad2deg(t2rad1);
-t2deg2 = rad2deg(t2rad2);
-
-k1 = l1 + l2*cos(t2rad1);
-k2 = l2*sin(t2rad1);
-t1rad1 = atan2(y,x) - atan2(k2,k1);
-t1deg1 = rad2deg(t1rad1);
-
-k1 = l1 + l2*cos(t2rad2);
-k2 = l2*sin(t2rad2);
-t1rad2 = atan2(y,x) - atan2(k2,k1);
-t1deg2 = rad2deg(t1rad2);
-
-%% generate points and compare
-xlow = -200;
-xhigh = 200;
-ylow = -200;
-yhigh = 200;
+%% randomly generate points and compare
+xlow = -200; xhigh = 200; ylow = -200; yhigh = 200;
 
 testSize = 100;
 xTestVec = (xhigh-xlow).*rand(testSize,1) + xlow;
@@ -47,13 +17,15 @@ yTestVec = (yhigh-ylow).*rand(testSize,1) + ylow;
 scatter(xTestVec,yTestVec);
 hold on
 didNotWork = 0;
-thetas = cell(1,100);
+thetas = cell(100,2);
 
 for testi = 1:testSize
+    x = xTestVec(testi);
+    y = yTestVec(testi);
     try
-        thetas{testi} = customIk(xTestVec(testi),yTestVec(testi));
-        scatter(xTestVec(testi),yTestVec(testi),'filled','d');
-        
+        thetas{testi,1} = customIk(x,y);
+        scatter(x,y,'filled','d');
+        thetas{testi,2} = customIkClosed(x,y,l1,l2);
         %compare variance here!!!
         
     catch
